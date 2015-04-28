336bed3987adfeb24b2ab9b2d394051bf3072ab3
mohsom <volodya.skalskyi@gmail.com>
2015-04-28 17:40:12 +0300

    Bubble sorting.

--- sorting/buble.js
+++ sorting/buble.js
@@ -1,19 +1,21 @@
 /**
  * Created by mohsom on 28.04.2015.
  */
-function sort(array){
-    var lngth=array.length;
-    for(var i=0;i<lngth;i++)
-    {
-        for(var j=length-1;j>i;j--){
-            if(array[j-1]>array[j]){
-                var tmp=array[i];
-                array[i]=array[j-1];
-                array[j-1]=tmp;
+(function () {
+    function sort(array) {
+        var lngth = array.length;
+        for (var i = 0; i < lngth; i++) {
+            for (var j = length - 1; j > i; j--) {
+                if (array[j - 1] > array[j]) {
+                    var tmp = array[i];
+                    array[i] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
             }
         }
+        return array;
     }
-    return array;
-}
-var a=[3,2,4,5,1];
-a.sort(); //1 2 3 4 5
+
+    var a = [3, 2, 4, 5, 1];
+    a.sort(); //1 2 3 4 5
+})();
