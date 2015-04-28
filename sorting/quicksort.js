2139cbe3578c4c8b46fb7f547583727ea55ad48d
mohsom <volodya.skalskyi@gmail.com>
2015-04-28 17:51:14 +0300

    Bubble sorting.

--- sorting/quicksort.js
+++ sorting/quicksort.js
@@ -1,3 +1,22 @@
 /**
  * Created by mohsom on 28.04.2015.
  */
+(function () {
+    function sort(array, length) {
+        var i = 0, j = length;
+        var p = array[parseInt(toString(j / 2))];
+        do {
+            while(a[i]<p) i++;
+            while(a[i]>p) j--;
+            if(i<=j){
+                var temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        } while (i <= j);
+        if ( j > 0 ) sort(array, j);
+        if ( length > i ) sort(array+i, j-i);
+    }
+    var a = [3, 2, 4, 5, 1];
+    a.sort(); //1 2 3 4 5
+})();
