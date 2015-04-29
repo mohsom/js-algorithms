7bba01eacb5a316c606ed36fd79f8eb8a765b18d
mohsom <volodya.skalskyi@gmail.com>
2015-04-29 20:11:29 +0300

    Queue.

--- data_structures/queue.js
+++ data_structures/queue.js
@@ -8,4 +8,28 @@
         $head = null;
         $tail = null;
     }
-})();
+
+    Queue.prototype.enqueue = function (elem) {
+        $queue.pop(elem);
+        $size++;
+    };
+    Queue.prototype.dequeue = function () {
+        if (!this.isEmpty()) {
+            var val = $queue[0];
+            $queue.splice(0,1);
+        }
+        else {
+            return null;
+        }
+    }
+    ;
+    Queue.prototype.isEmpty = function () {
+        if ($size === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+})
+();
