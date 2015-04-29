46749d93d2e35d764a159a02714c8a691648bfda
mohsom <volodya.skalskyi@gmail.com>
2015-04-29 17:35:07 +0300

    Stack.

--- data_structures/stack.js
+++ data_structures/stack.js
@@ -1,3 +1,16 @@
 /**
  * Created by mohsom on 29.04.2015.
  */
+(function(){
+    function Stack(){
+        $stack=[];
+        $size=0;
+    }
+    Stack.prototype.push=function(elem){
+        $stack.push(elem);
+        $size++;
+    };
+    Stack.prototype.pop=function(){
+
+    }
+})();
