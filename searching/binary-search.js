f23a6cf2aaebfef75c8e43614b07c8daecb62f00
mohsom <volodya.skalskyi@gmail.com>
2015-05-01 19:27:42 +0300

    Binary search.

--- searching/binary-search.js
+++ searching/binary-search.js
@@ -6,12 +6,14 @@
         var left = 0,
             right = array.length,
             mid;
+        if((key>array[array.length-1])||(key<array[0])){
+            return null;
+        }
         while (left <= right) {
             mid = left + (right - left) / 2;
             if (key < array[mid]) right = mid - 1;
             else if (key > array[mid]) left = mid + 1;
             else return mid;
         }
-        return null;
     }
 })();
