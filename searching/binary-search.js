4806b2590d6b989f1adde1ae63e884904c660fa0
mohsom <volodya.skalskyi@gmail.com>
2015-05-01 19:19:43 +0300

    Binary search.

--- searching/binary-search.js
+++ searching/binary-search.js
@@ -1,3 +1,17 @@
 /**
  * Created by mohsom on 01.05.2015.
  */
+(function () {
+    function search(array, key) {
+        var left = 0,
+            right = array.length,
+            mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (key < array[mid]) right = mid - 1;
+            else if (key > array[mid]) left = mid + 1;
+            else return mid;
+        }
+        return null;
+    }
+})();
