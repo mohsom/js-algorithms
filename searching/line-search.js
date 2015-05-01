c9482ca32887705459ddbd6c692ef1d857ffc69a
mohsom <volodya.skalskyi@gmail.com>
2015-05-01 18:30:51 +0300

    Line search.

--- searching/line-search.js
+++ searching/line-search.js
@@ -3,6 +3,12 @@
  */
 (function () {
     function search(array, key) {
-              
+        var length = array.length;
+        for(var i=0;i<length;i++){
+            if(a[i]===key){
+                return i;
+            }
+        }
+        return null;
     }
 })();
